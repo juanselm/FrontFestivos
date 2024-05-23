f34a29bd4ab2f226d2a0e0f24629ee33e777339b
Juan Loaiza <juanmazo1234@gmail.com>
2024-05-22 19:23:35 -0500

    se eliminan clases inecesarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { AppComponent } from './app.component';
-import { YearCalendarComponent } from './features/components/year-calendar/year-calendar.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    YearCalendarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    YearCalendarComponent,
     CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
   providers: [],
